95edad515504a60787f1f4e76805104a02cdef77
Armaniimus@laptop <tommy_chester@hotmail.com>
2025-05-10 23:59:13 +0200

    added player objects and made them move

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
@@ -10,17 +11,33 @@ def main():
 
 	clock = pygame.time.Clock()
 	dt = 0
+
+	updateable_players = pygame.sprite.Group()
+	drawable_players = pygame.sprite.Group()
+	Player.containers = (updateable_players, drawable_players)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+	# updateable.
+
+
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
-	while True:
+	
 
+	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
+		updateable_players.update(dt)
+		# player.update(dt)
+
 		screen.fill("black")
+		for player in drawable_players:
+			player.draw(screen)
+
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
